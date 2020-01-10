a26fbae28ba5ae0eb5e3bd1f44c2f46091e02e33
U0190410 [Alejandro] <alejandro.gonzalez.g@proveedorexterno.com>
2020-01-10 14:47:12 +0100

    PLOTS

--- bot/respuestas.py
+++ bot/respuestas.py
@@ -1,4 +1,4 @@
-
+import matplotlib.pyplot as plt
 
 class Respuestas(object):
 
@@ -23,3 +23,75 @@ class Respuestas(object):
                         value: 1
                 }
 
+
+    def _get_data_plot(self, pregunta_id):
+        if pregunta_id in self.resultados:
+            labels = []
+            data = []
+            for valor, count in self.resultados[pregunta_id].items():
+                labels.append(valor)
+                data.append(labels)
+                return {
+                    'labels': labels,
+                    'data': data
+                }
+        else:
+            return None
+
+
+
+    def get_pie(self, pregunta_id):
+        raw = self._get_data_plot(pregunta_id)
+        if raw:
+            labels = raw['labels']
+            data = raw['data']
+            plt.pie(data,
+                    labels=labels,
+                    explode=(0.05, 0.05, 0.05),
+                    shadow=True,
+                    autopct='%1.1f%%')
+            path = '../img/{id}_{pregunta}_pie.png'.format(
+                    id=self.id,
+                    pregunta=pregunta_id
+            )
+            plt.savefig(path)
+            return path
+        else:
+            return None
+
+
+
+    def get_bar(self, pregunta_id):
+        raw = self._get_data_plot(pregunta_id)
+        if raw:
+            labels = tuple(raw['labels'])
+            data = raw['data']
+            index = list(range(len(data)))
+            x = [5, 3, 2]
+            plt.bar(index, data)
+            plt.xticks(index, labels)
+            path = '../img/{id}_{pregunta}_bar.png'.format(
+                id=self.id,
+                pregunta=pregunta_id
+            )
+            plt.savefig(path)
+            return path
+        else:
+            return None
+
+
+    def repost(self):
+        template ='{pregunta} {resposta} {count}'
+        salida = template.format(
+            pregunta='*PREGUNTA*'.rjust(10, ' '),
+            resposta='*VALOR*'.rjust(10, ' '),
+            count='*RESPOSTES*'.rjust(10, ' ')
+        )
+        for preg, resp in self.resultados.items():
+            for valor, count in resp.items():
+                salida += template.format(
+                    pregunta=preg.rjust(10, ' '),
+                    resposta=valor.rjust(10, ' '),
+                    count=count.rjust(10, ' ')
+                )
+        return salida
